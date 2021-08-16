af57906822c9a9fa41f02b0d60e3ad24c731a1c0
Mike Gurney <mike.gurney@codemettle.com>
2021-08-16 10:19:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require("http");
+
+var options = {
+  host: "ipaddress.fyi",
+};
+
+function getPublicIp() {
+  return new Promise((resolve, reject) => {
+    const options = {
+      host: "ipaddress.fyi",
+      method: "GET",
+    };
+    const req = http.request(options, (res) => {
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        return reject(new Error("statusCode=" + res.statusCode));
+      }
+      var ret = "";
+      res.on("data", function (chunk) {
+        ret += chunk;
+      });
+      res.on("end", function () {
+        resolve(ret);
+      });
+    });
+    req.on("error", (e) => {
+      reject(e.message);
+    });
+    req.end();
+  });
+}
